62a2e41e0497f699a262451dfcdb3341cc6bb4a9
Micrah <mic_24@live.com>
2019-04-14 14:30:22 +1000

    typo

--- mod-anticheat/src/AnticheatScripts.cpp
+++ mod-anticheat/src/AnticheatScripts.cpp
@@ -39,7 +39,7 @@ public:
 			sLog->outString( "Anticheat: Resetting daily report states.");
 			sAnticheatMgr->ResetDailyReportStates();
 			UpdateReportResetTime();
-			sLog->outString( "Anticheat: Next daily report reset: %li", resetTime);
+			sLog->outString( "Anticheat: Next daily report reset: %ld", resetTime);
 		}
 		if (sWorld->GetUptime() > lastIterationPlayer)
 		{
